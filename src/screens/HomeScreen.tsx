b2cfa4b2a8a731f152fb6dfe5265275f08bd37c9
Mauri <mauri.monzon91@gmail.com>
2023-09-04 15:32:59 -0300

    MenuItems del screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,36 @@
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+interface MenuItem {
+  name: string;
+  icon: string;
+  components: string;
+}
+const menuItems = [
+  {name: 'Animación 1', icon: 'cube-outline', components: 'Animation101Screen'},
+];
+
 const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text style={{color: 'black'}}>
+          {menuItem.name} - {menuItem.icon}
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text style={{color: 'black'}}>HomeScreen</Text>
-      <Icon name="star-outline" size={50} color={'black'} />
+    <View style={{flex: 1}}>
+      <FlatList
+        data={menuItems}
+        // Renderizar cada uno de los elementos - Como un map
+        renderItem={({item}) => renderMenuItem(item)}
+        // Llave unica en cada uno de los elementos
+        keyExtractor={item => item.name}
+      />
     </View>
   );
 };
