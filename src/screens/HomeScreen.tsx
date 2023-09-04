7804575f9e7661b4099d7ca2b8cc42556cf7656a
Mauri <mauri.monzon91@gmail.com>
2023-09-04 15:44:32 -0300

    ListHeader

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,35 +1,53 @@
 import {View, Text, FlatList} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {styles} from '../theme/appTheme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 interface MenuItem {
   name: string;
   icon: string;
   components: string;
 }
+
 const menuItems = [
   {name: 'Animación 1', icon: 'cube-outline', components: 'Animation101Screen'},
+  {
+    name: 'Animación 2',
+    icon: 'game-controller-outline',
+    components: 'Animation102Screen',
+  },
 ];
 
 const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
   const renderMenuItem = (menuItem: MenuItem) => {
     return (
       <View>
-        <Text style={{color: 'black'}}>
+        <Text style={styles.parrafo}>
           {menuItem.name} - {menuItem.icon}
         </Text>
       </View>
     );
   };
 
+  const renderListHeader = () => {
+    return (
+      <View style={{marginTop: top + 20}}>
+        <Text style={styles.title}>Opciones de menú</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={{flex: 1}}>
+    <View style={{flex: 1, ...styles.globalMargin}}>
       <FlatList
         data={menuItems}
         // Renderizar cada uno de los elementos - Como un map
         renderItem={({item}) => renderMenuItem(item)}
         // Llave unica en cada uno de los elementos
         keyExtractor={item => item.name}
+        ListHeaderComponent={() => renderListHeader()}
       />
     </View>
   );
