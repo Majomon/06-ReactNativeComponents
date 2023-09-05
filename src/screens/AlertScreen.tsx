74eed54320aca7ca2335942a68c967632e9d2e03
Mauri <mauri.monzon91@gmail.com>
2023-09-05 15:55:37 -0300

    showPrompt para android

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Alert, Button, View} from 'react-native';
 import HeaderTitle from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
+import prompt from 'react-native-prompt-android';
 
 export const AlertScreen = () => {
   const showAlert = () => {
@@ -25,10 +26,36 @@ export const AlertScreen = () => {
       },
     );
   };
+
+  const showPrompt = () => {
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: password => console.log('OK Pressed, password: ' + password),
+        },
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder',
+      },
+    );
+  };
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="Alerts" />
-      <Button title="Mostrar alerta" onPress={showAlert} />
+      <Button title="Mostrar Alerta" onPress={showAlert} />
+      <View style={{height: 10}} />
+      <Button title="Mostrar Prompt" onPress={showPrompt} />
     </View>
   );
 };
