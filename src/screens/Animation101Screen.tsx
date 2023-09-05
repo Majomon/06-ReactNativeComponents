0cd4b329cf010ec79f11f90be3fdbcd9834896cd
Mauri <mauri.monzon91@gmail.com>
2023-09-05 09:57:04 -0300

    Hecho fadeOut

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -2,7 +2,7 @@ import React, {useRef} from 'react';
 import {Animated, Button, StyleSheet, View} from 'react-native';
 
 const Animation101Screen = () => {
-  const opacity = useRef(new Animated.Value(0.4)).current;
+  const opacity = useRef(new Animated.Value(0)).current;
 
   const fadeIn = () => {
     Animated.timing(opacity, {
@@ -14,10 +14,25 @@ const Animation101Screen = () => {
     }).start();
   };
 
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      //Valor al que necesito que llegue
+      toValue: 0,
+      //Lo que va a durar :D en milisegundos
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <View style={styles.container}>
       <Animated.View style={{...styles.purpleBox, opacity, marginBottom: 20}} />
-      <Button title="FaseIn" onPress={fadeIn} />
+      <View style={styles.btn}>
+        <Button title="FaseIn" onPress={fadeIn} />
+      </View>
+      <View style={styles.btn}>
+        <Button title="FaseOut" onPress={fadeOut} />
+      </View>
     </View>
   );
 };
@@ -34,5 +49,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#5856D6',
     width: 150,
     height: 150,
+  },
+  btn: {
+    width: 150,
+    height: 50,
+    marginVertical: 10,
   },
 });
