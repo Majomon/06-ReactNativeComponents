75d72114d38c68e6475d24007231a51c5a00df1a
Mauri <mauri.monzon91@gmail.com>
2023-09-05 09:54:27 -0300

    Hecho fadeIn

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,12 +1,23 @@
 import React, {useRef} from 'react';
-import {Animated, StyleSheet, View} from 'react-native';
+import {Animated, Button, StyleSheet, View} from 'react-native';
 
 const Animation101Screen = () => {
   const opacity = useRef(new Animated.Value(0.4)).current;
 
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      //Valor al que necesito que llegue
+      toValue: 1,
+      //Lo que va a durar :D en milisegundos
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <View style={styles.container}>
-      <Animated.View style={{...styles.purpleBox, opacity}} />
+      <Animated.View style={{...styles.purpleBox, opacity, marginBottom: 20}} />
+      <Button title="FaseIn" onPress={fadeIn} />
     </View>
   );
 };
