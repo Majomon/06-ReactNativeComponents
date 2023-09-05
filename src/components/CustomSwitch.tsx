872ec96ec3903af45f4167f25773d966206585ad
Mauri <mauri.monzon91@gmail.com>
2023-09-05 15:10:01 -0300

    Switch con validaciones simil formulario

---
+++ src/components/CustomSwitch.tsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import {Switch} from 'react-native';
+
+interface Props {
+  isOn: boolean;
+  onChange: (value: boolean) => void;
+}
+
+export const CustomSwitch = ({isOn, onChange}: Props) => {
+  const [isEnabled, setIsEnabled] = useState(isOn);
+
+  const toggleSwitch = () => {
+    setIsEnabled(!isEnabled);
+    onChange(!isEnabled);
+  };
+
+  return (
+    <Switch
+      trackColor={{false: '#D9D9DB', true: '#5658D6'}}
+      thumbColor={isEnabled ? '#5658D6' : '#f4f3f4'}
+      //ios_backgroundColor="#3e3e3e"
+      onValueChange={toggleSwitch}
+      value={isEnabled}
+    />
+  );
+};
--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -1,30 +1,47 @@
 import React, {useState} from 'react';
-import {View, Switch, StyleSheet} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {CustomSwitch} from '../components/CustomSwitch';
 import HeaderTitle from '../components/HeaderTitle';
 
 export const SwitchScreen = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(!isEnabled);
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
+  const {isActive, isHappy, isHungry} = state;
+  const onChange = (value: boolean, field: string) => {
+    setState({...state, [field]: value});
+  };
 
   return (
     <View style={styles.container}>
       <HeaderTitle title="Switches" />
-      <Switch
-        trackColor={{false: '#D9D9DB', true: '#5658D6'}}
-        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-        //ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
+      <View style={styles.switchRow}>
+        <Text style={styles.infoSwitch}>Is active</Text>
+        <CustomSwitch
+          isOn={isActive}
+          onChange={value => onChange(value, 'isActive')}
+        />
+      </View>
+
+      <Text style={styles.infoSwitch}>{JSON.stringify(state, null, 5)}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    /*     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center', */
     marginHorizontal: 20,
+  },
+  switchRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  infoSwitch: {
+    color: 'black',
+    fontSize: 25,
   },
 });
