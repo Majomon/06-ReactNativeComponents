bbdabe196ff3b7173110ab0fb5ecc30a440c8c52
Mauri <mauri.monzon91@gmail.com>
2023-09-05 16:17:10 -0300

    Text inputs

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {TextInput, View, StyleSheet} from 'react-native';
+import {} from 'react-native-gesture-handler';
 import HeaderTitle from '../components/HeaderTitle';
+import {styles} from '../theme/appTheme';
 
 export const TextInputScreen = () => {
   return (
-    <View>
+    <View style={styles.globalMargin}>
       <HeaderTitle title="TextInputs" />
+      <TextInput style={stylesScreen.textInput} />
     </View>
   );
 };
+
+const stylesScreen = StyleSheet.create({
+  textInput: {
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'rgba(0,0,0,0.3)',
+    padding: 10,
+  },
+});
