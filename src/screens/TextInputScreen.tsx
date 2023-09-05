cfa82532d64a03db841c06826da12aef41648628
Mauri <mauri.monzon91@gmail.com>
2023-09-05 16:36:00 -0300

    Propiedades de los inputs

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TextInput, View, StyleSheet} from 'react-native';
-import {} from 'react-native-gesture-handler';
 import HeaderTitle from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
 
 export const TextInputScreen = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+
+  const onChange = (value: string, field: string) => {
+    setForm({...form, [field]: value});
+  };
+
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="TextInputs" />
-      <TextInput style={stylesScreen.textInput} />
+      <TextInput
+        style={stylesScreen.textInput}
+        placeholder="Ingrese su nombre"
+        placeholderTextColor="black"
+        //Para que no me marque error de autocompletado
+        autoCorrect={false}
+        autoCapitalize="words"
+        onChangeText={value => onChange(value, 'name')}
+      />
+      <TextInput
+        style={stylesScreen.textInput}
+        placeholder="Ingrese su email"
+        placeholderTextColor="black"
+        autoCorrect={false}
+        autoCapitalize="none"
+        onChangeText={value => onChange(value, 'email')}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={stylesScreen.textInput}
+        placeholder="Ingrese su teléfono"
+        placeholderTextColor="black"
+        onChangeText={value => onChange(value, 'phone')}
+        keyboardType="phone-pad"
+
+      />
+      <HeaderTitle title={JSON.stringify(form, null, 3)} />
     </View>
   );
 };
 
 const stylesScreen = StyleSheet.create({
   textInput: {
+    color: 'black',
+    opacity: 0.7,
     height: 40,
     borderWidth: 1,
     borderRadius: 5,
-    borderColor: 'rgba(0,0,0,0.3)',
+    /*    borderColor: 'rgba(0,0,0,0.3)', */
     padding: 10,
+    marginVertical: 5,
   },
 });
