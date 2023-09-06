08a4ea414d2f84c6a02bd9345e6939359c859991
Mauri <mauri.monzon91@gmail.com>
2023-09-05 22:34:37 -0300

    Aplicando algunas propiedades del refreshControl

--- src/screens/PullToRefreshScreen.tsx
+++ src/screens/PullToRefreshScreen.tsx
@@ -1,28 +1,45 @@
 import React, {useState} from 'react';
-import {ScrollView, StyleSheet, View, RefreshControl} from 'react-native';
+import {ScrollView, StyleSheet, View, RefreshControl, Text} from 'react-native';
 import HeaderTitle from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
 
 export const PullToRefreshScreen = () => {
   const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState<string>();
 
   const onRefresh = () => {
     setRefreshing(true);
     setTimeout(() => {
       console.log('Terminamos');
       setRefreshing(false);
-    }, 1500);
+      setData('Hola mundo!');
+    }, 3000);
   };
   return (
     <ScrollView
       refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          //Hasta donde va a bajar en el eje Y el loading
+          progressViewOffset={10}
+          //Color del círculo
+          progressBackgroundColor="grey"
+          //Gradiente de colores
+          colors={['white', 'yellow', 'orange']}
+        />
       }>
       <View style={styles.globalMargin}>
         <HeaderTitle title="Pull to refresh" />
+        {data && <HeaderTitle title={data} />}
       </View>
     </ScrollView>
   );
 };
 
-const stylesPull = StyleSheet.create({});
+const stylesPull = StyleSheet.create({
+  text: {
+    color: 'black',
+    fontSize: 15,
+  },
+});
