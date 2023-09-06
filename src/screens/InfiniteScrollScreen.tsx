6931f4e18f88883e0f0ecb7ceb241c68c9b2e42a
Mauri <mauri.monzon91@gmail.com>
2023-09-06 09:58:38 -0300

    Scroll infinito con imagenes

--- src/screens/InfiniteScrollScreen.tsx
+++ src/screens/InfiniteScrollScreen.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {FlatList, Image, StyleSheet, Text, View} from 'react-native';
 import HeaderTitle from '../components/HeaderTitle';
+import {ActivityIndicator} from 'react-native';
 
 export const InfiniteScrollScreen = () => {
   const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
@@ -10,10 +11,21 @@ export const InfiniteScrollScreen = () => {
     for (let i = 0; i < 5; i++) {
       newArray[i] = numbers.length + i;
     }
-    setNumbers([...numbers, ...newArray]);
+    // Simulando retraso en la petición
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 1500);
   };
   const renderItem = (item: number) => {
-    return <Text style={styles.textItem}>{item}</Text>;
+    return (
+      <Image
+        source={{uri: `https://picsum.photos/id/${item}/500/400`}}
+        style={{
+          width: '100%',
+          height: 400,
+        }}
+      />
+    );
   };
 
   return (
@@ -27,6 +39,17 @@ export const InfiniteScrollScreen = () => {
         onEndReached={loadMore}
         // Que tan cerca del fondo debo estar para disparar nuevamente esta función -> onEndReached
         onEndReachedThreshold={0.5}
+        ListFooterComponent={() => (
+          <View
+            style={{
+              height: 150,
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <ActivityIndicator size={30} color={'#5856D6'} />
+          </View>
+        )}
       />
     </View>
   );
