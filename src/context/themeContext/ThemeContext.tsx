805b142114b15ea5918a057e21d68de95275faac
Mauri <mauri.monzon91@gmail.com>
2023-09-06 23:44:10 -0300

    Listo

--- src/context/themeContext/ThemeContext.tsx
+++ src/context/themeContext/ThemeContext.tsx
@@ -1,5 +1,6 @@
-import React, {createContext, useReducer} from 'react';
-import {ThemeState, lightTheme, themeReducer} from './themeReducer';
+import React, {createContext, useReducer, useEffect} from 'react';
+import {useColorScheme, AppState, Appearance} from 'react-native';
+import {ThemeState, darkTheme, lightTheme, themeReducer} from './themeReducer';
 
 interface ThemeContextProps {
   theme: ThemeState;
@@ -10,7 +11,32 @@ interface ThemeContextProps {
 export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({children}: any) => {
-  const [theme, dispatch] = useReducer(themeReducer, lightTheme);
+  //Solo funciona en IOS
+  //const colorScheme = useColorScheme();
+
+  const [theme, dispatch] = useReducer(
+    themeReducer,
+    Appearance.getColorScheme() === 'dark' ? darkTheme : lightTheme,
+  );
+
+  //Esto solo funciona en IOS
+  /*   useEffect(() => {
+    colorScheme === 'light' ? setLightTheme() : setDarkTheme();
+  }, [colorScheme]); */
+
+  // Esto funciona en Android
+  useEffect(() => {
+    AppState.addEventListener('change', status => {
+      //Puedo ver si esta activo o si esta en el background
+      //console.log(status);
+      //status="" Para ver las opciones
+      if (status === 'active') {
+        Appearance.getColorScheme() === 'light'
+          ? setLightTheme()
+          : setDarkTheme();
+      }
+    });
+  }, []);
 
   const setDarkTheme = () => {
     dispatch({type: 'set_dark_theme'});
